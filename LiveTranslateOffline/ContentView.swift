8eeac3aef544757499820835b843c03d6cfeb229
Nirbhay Singh Narang <nsn8@cornell.edu>
2025-01-08 04:05:52 +0530

    Initial Commit

---
+++ LiveTranslateOffline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveTranslateOffline
+//
+//  Created by Nirbhay Singh Narang on 1/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveTranslateOffline/LiveTranslateOfflineApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveTranslateOfflineApp.swift
+//  LiveTranslateOffline
+//
+//  Created by Nirbhay Singh Narang on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveTranslateOfflineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
